17475f0ff7502222555cf40471cce98bb7346892
Javier Pallarés González <javpalgon@alum.us.es>
2026-03-18 18:02:38 +0100

    fix: compilation error and docker compose structure
    We've added an import to make the app compilate and we changed docker-compose file because in dev it should us h2, instead of using azure db

--- src/main/java/com/streetask/app/user/UserService.java
+++ src/main/java/com/streetask/app/user/UserService.java
@@ -17,6 +17,7 @@ import com.streetask.app.exceptions.ResourceNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.streetask.app.question.QuestionRepository;
